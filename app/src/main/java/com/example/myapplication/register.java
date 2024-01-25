1c679cc6297ae0dcbdf5fd8bcce92b93f2a9b0e1
You <blessingmalaza.1@gmail.com>
2024-01-25 22:14:27 +0200

    final draft

--- app/src/main/java/com/example/myapplication/register.java
+++ app/src/main/java/com/example/myapplication/register.java
@@ -2,15 +2,23 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class register extends AppCompatActivity {
     private Button btn,button;
+    TextInputEditText editTextemail,editTextpassword;
+    Button hod;
+    FirebaseAuth firebaseAuth =  FirebaseAuth.getInstance();
 
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +33,23 @@ public class register extends AppCompatActivity {
                                    }
                                }
         );
+
+
+        editTextemail = findViewById(R.id.email);
+        editTextpassword = findViewById(R.id.password);
+
+         hod=findViewById(R.id.hodButton);
+
+         hod.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(register.this, hoddetails.class);
+                 startActivity(intent);
+                 finish();
+             }
+         });
+
+
     }
 
     public void StudentClick(View view) {
@@ -32,5 +57,8 @@ public class register extends AppCompatActivity {
         Intent intent = new Intent(register.this, studentregistration.class);
                                     startActivity(intent);
     }
+
+
+
 
 }
--- app/src/main/java/com/example/myapplication/sign_in.java
+++ app/src/main/java/com/example/myapplication/sign_in.java
@@ -1,20 +1,35 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class sign_in extends AppCompatActivity {
 
     private Button btn;
+    TextInputEditText editTextemail,editTextpassword;
+Button login;
+Button student;
+FirebaseAuth firebaseAuth =  FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        login= findViewById(R.id.loginButton);
 
         btn = (Button) findViewById(R.id.create_button);
 
@@ -23,9 +38,42 @@ public class sign_in extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(sign_in.this, register.class);
                 startActivity(intent);
+                login.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String email, password;
+                        email = String.valueOf(editTextemail.getText());
+                        password= String.valueOf(editTextpassword.getText());
+
+                        if(TextUtils.isEmpty(email)){
+                            Toast.makeText(sign_in.this,"Enter Email",Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        if(TextUtils.isEmpty(password)){
+                            Toast.makeText(sign_in.this,"Enter Password",Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        firebaseAuth.signInWithEmailAndPassword(email,password)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if(task.isSuccessful()){
+                                            Toast.makeText(sign_in.this,"login successful",Toast.LENGTH_SHORT).show();
+                                            Intent intent = new Intent(sign_in.this,register.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                        else {
+                                            Toast.makeText(sign_in.this,"login unsuccessful",Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                    }
+                });
             }
         }
         );
+
     }
 
 }
