d4dd5cb960405b0ddacf59cfed36251406cf0fa7
You <blessingmalaza.1@gmail.com>
2024-01-31 23:02:02 +0200

    Temaphovela

--- app/src/main/java/com/example/myapplication/results.java
+++ app/src/main/java/com/example/myapplication/results.java
@@ -6,24 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import androidx.annotation.NonNull;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class results extends AppCompatActivity {
-    private EditText subject1GradeEditText;
-    private EditText subject2GradeEditText;
-    // Add more EditText fields for additional subjects if needed
-
-    private DatabaseReference databaseReference;
-
+    private EditText editTextSubject1, editTextGrade1, editTextSubject2, editTextGrade2;
     private Button buttonUploadResults;
 
     Button next;
